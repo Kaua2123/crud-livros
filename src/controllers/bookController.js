962b20d68ed434a318996bd8f030905267f4a40c
Kaua2123 <kauantnetsoares@gmail.com>
2023-12-26 07:15:57 -0300

    CRUD - C -> Criação de livros funcionando

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,7 +1,24 @@
+const BookModel = require('../models/BookModel');
+
 exports.bookIndex = async function(req, res) {
     return res.render('cadastro-livros');
 }
 
 exports.postBook = async function(req, res) {
-    console.log(req.body);
+    const book = new BookModel(req.body);
+    await book.createBook();
+
+    if(book.errors.length > 0) {
+        req.flash('errors', book.errors);
+        req.session.save(() => {
+            res.redirect('/book/index')
+        });
+        return;
+    }
+
+    req.flash('success', 'Seu livro foi cadastrado com sucesso.');
+    req.session.save(() => {
+        res.redirect('/book/index')
+    });
+    return;
 }
--- src/models/BookModel.js
+++ src/models/BookModel.js
@@ -12,10 +12,29 @@ const BookModel = mongoose.model('Books', BookSchema);
 
 class Book {
     constructor(body) {
+        this.body = body;
+        this.nome = body.nome;
+        this.genero = body.genero
+        this.autor = body.autor;
+        this.editora = body.editora;
+        this.errors = [];
+    }
+
+    async createBook() {
+        this.valida();
+
+        const buscaLivro = await BookModel.findOne({ nome: this.nome });
+        if (buscaLivro != null) this.errors.push('Já existe um livro com esse nome.');
+        if (this.errors.length > 0) return;
+        
+        await BookModel.create({ nome: this.nome, genero: this.genero, autor: this.autor, editora: this.editora});
+    }
+
+    valida() {
+        if (!this.nome || !this.genero || !this.autor || !this.editora) this.errors.push('Todos os campos são obrigatórios.');
+        if (this.errors.length > 0) return;
         
     }
-
-    createBook() {
-
-    }
 }
+
+module.exports = Book;
